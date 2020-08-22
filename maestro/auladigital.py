13d725d5b0482dcb430ef9cfa53f1f3d0312948d
pbittencourt <pedrogabrieltt@gmail.com>
2020-08-22 13:51:25 -0300

    função errorquit() para lidar com exceções

--- maestro/auladigital.py
+++ maestro/auladigital.py
@@ -16,6 +16,7 @@ from csv import reader
 import logging
 import getpass
 import os
+import sys
 
 
 def login(username: str, password: str) -> None:
@@ -45,8 +46,22 @@ def login(username: str, password: str) -> None:
         return False
 
 
+def errorquit(msg: str = 'ERRO NÃO ESPECIFICADO') -> str:
+    """
+    Quando falhar numa requisição importante, exibe
+    mensagem de erro, registra no log, fecha o driver
+    e encerra o programa.
+    """
+    logger.error(f'Não foi possível continuar devido ao seguinte problema: {msg}.')
+    logger.info('O programa será encerrado agora. Verifique o registro para detalhes!')
+    driver.quit()
+    sys.exit(1)
+
+
 # globals
 turmas = {
+    '5A': ('5º Ano EF', '5º ANO A'),
+    '5B': ('5º Ano EF', '5º ANO B'),
     '6A': ('6º Ano EF', '6º ANO A'),
     '6B': ('6º Ano EF', '6º ANO B'),
     '7A': ('7º Ano EF', '7º ANO 2020'),
@@ -54,13 +69,15 @@ turmas = {
     '9A': ('9º Ano EF', '9º ANO 2020'),
     '1A': ('1ª Série EM', '1º MÉDIO 2020'),
     '2A': ('2ª Série EM', '2º MÉDIO 2020'),
-    '3A': ('3ª Série EM', '3º MÉDIO 2020')
+    '3A': ('3ª Série EM', '3º MÉDIO 2020'),
 }
 
 disciplinas = {
     'DG': 'Desenho Geométrico',
     'FIS': 'Física',
-    'GMT': 'Matemática'
+    'GMT': 'Matemática',
+    'CIE': 'Ciências',
+    'QUI': 'Química',
 }
 
 ##############################
@@ -96,7 +113,7 @@ username = str(input('Usuário: _ '))
 password = getpass.getpass('Senha: _ ')
 
 # inicializa driver
-driver = webdriver.Firefox(executable_path=os.getcwd() + r'/drivers/geckodriver')
+driver = webdriver.Chrome(executable_path=os.getcwd() + r'/../drivers/chromedriver')
 
 # verifica login
 if login(username, password):
@@ -111,9 +128,8 @@ if login(username, password):
         driver.find_element_by_xpath("//*[@aria-label = 'Professor']").click()
         logger.info('Perfil de professor selecionado.')
     except:
-        # entrou direto na conta de professor
-        logger.error('Entrou direto na conta ou deu erro. Analisar!!')
-    sleep(15)
+        errorquit('não foi possível selecionar perfil de professor')
+    sleep(10)
 
     # le csv contendo aulas para criar
     with open(os.getcwd() + '/auladigital.csv') as file:
@@ -122,15 +138,13 @@ if login(username, password):
         for row in handle:
             if line_count > 0:
                 turma, disciplina, titulo, dia, horario = row
-                success = True
                 logger.info(f'Inserindo registro {line_count} ...')
 
                 # abre menu de séries
                 try:
-                    driver.find_element_by_xpath("//*[@aria-label='Ano/Serie']").click()
+                    driver.find_element_by_xpath("//*[@aria-label='Ano/Serir']").click()
                 except:
-                    logger.error(f'Não foi possível abrir o menu de séries.')
-                    success = False
+                    errorquit('não foi possível abrir o menu de séries')
                 sleep(2)
 
                 # seleciona a série desejada
@@ -139,16 +153,14 @@ if login(username, password):
                     driver.find_element_by_xpath("//*[@aria-label = '" + sel_serie + "']").click()
                     logger.info(f'Selecionou a série {sel_serie}.')
                 except:
-                    logger.error(f'Não foi possível selecionar a série {sel_serie}.')
-                    success = False
+                    errorquit(f'não foi possível selecionar a série {sel_serie}')
                 sleep(3)
 
                 # abre menu de turmas/disciplinas
                 try:
                     driver.find_element_by_xpath("//*[@aria-label='Turma/Disciplina']").click()
                 except:
-                    logger.error(f'Não foi possível abrir menu de disciplinas.')
-                    success = False
+                    errorquit(f'não foi possível abrir menu de disciplinas')
                 sleep(2)
 
                 # seleciona a turma/disciplina desejada
@@ -157,8 +169,7 @@ if login(username, password):
                     driver.find_element_by_xpath("//*[@aria-label = '" + sel_disciplina + "']").click()
                     logger.info(f'Selecinou a disciplina {sel_disciplina}.')
                 except:
-                    logger.error(f'Não foi possível selecionar a disciplina {sel_disciplina}.')
-                    success = False
+                    errorquit(f'não foi possível selecionar a disciplina {sel_disciplina}')
                 sleep(3)
 
                 # clica em criar nova aula
@@ -166,8 +177,7 @@ if login(username, password):
                     driver.find_element_by_xpath("/html/body/div[3]/div[2]/md-content/div/div/div[1]/div[1]/div[2]/div[3]/button").click()
                     logger.info('Clicou em criar nova aula.')
                 except:
-                    logger.error('Não foi possível clicar em criar nova aula.')
-                    success = False
+                    errorquit('não foi possível clicar em criar nova aula')
                 sleep(3)
 
                 # título da aula: limpa e seta valor
@@ -177,8 +187,7 @@ if login(username, password):
                     aula_titulo.send_keys(titulo)
                     logger.info(f'Inseriu título da aula: {titulo}.')
                 except:
-                    logger.error(f'Não foi possível inserir título da aula: {titulo}.')
-                    success = False
+                    errorquit(f'não foi possível inserir título da aula: {titulo}')
                 sleep(2)
 
                 # dia da aula: limpa e seta valor
@@ -188,8 +197,7 @@ if login(username, password):
                     aula_dia.send_keys(dia)
                     logger.info(f'Inseriu a data {dia}.')
                 except:
-                    logger.error(f'Não foi possível inserir a data {dia}.')
-                    success = False
+                    errorquit(f'não foi possível inserir a data {dia}')
                 sleep(2)
 
                 # horário da aula: limpa e seta valor
@@ -199,22 +207,23 @@ if login(username, password):
                     aula_horario.send_keys(horario)
                     logger.info(f'Inseriu o horário {horario}.')
                 except:
-                    logger.error(f'Não foi possível inserir o horário {horario}.')
-                    success = False
+                    errorquit(f'não foi possível inserir o horário {horario}')
                 sleep(2)
 
                 # salva a aula
-                if success:
-                    try:
-                        #driver.find_element_by_xpath("//*[@ng-click='save()']").click()
-                        logger.info('Aula adicionada com sucesso!')
-                    except:
-                        logger.warning('Houve algum erro ao salvar a aula.')
-                else:
-                    logger.warning('Não foi possível adicionar a aula por falha em uma ou mais requisições.')
+                try:
+                    driver.find_element_by_xpath("//*[@ng-click='save()']").click()
+                    logger.info('Aula adicionada com sucesso!')
+                except:
+                    logger.warning('Houve algum erro ao salvar a aula.')
 
                 sleep(30)
                 
             line_count += 1
 
     logger.info(f'PROCESSO FINALIZADO!\n' +  '='*40)
+else:
+    logger.error('Encerrando programa devido ausência de credenciais.')
+
+# encerra driver
+driver.quit()
